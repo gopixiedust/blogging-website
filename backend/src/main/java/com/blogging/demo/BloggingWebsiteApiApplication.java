9696d8b898951fcb2547c4f53cd40f9332437eca
gopixiedust <genjutsumaster10@gmail.com>
2023-06-12 00:01:14 +0530

    add the user schema

---
+++ backend/src/main/java/com/blogging/demo/BloggingWebsiteApiApplication.java
@@ -0,0 +1,13 @@
+package com.blogging.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BloggingWebsiteApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BloggingWebsiteApiApplication.class, args);
+	}
+
+}
